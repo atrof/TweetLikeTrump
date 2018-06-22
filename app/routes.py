6c07c0c70b0f04260c5578cba59b6dee703641ea
avtrr <avtrrr@gmail.com>
2018-06-22 14:28:33 +0700

    OneClass SVM deployed

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from sklearn import svm
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.externals import joblib
+
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import TweetForm
 from app import app
 
@@ -6,8 +11,8 @@ from app import app
 @app.route('/index')
 def index():
 	user = {'username': 'Trump Imitator'}
-	form = TweetForm()
-	return render_template('index.html', title='TweetLikeTrump', user=user, form=form)
+	#form = TweetForm()
+	return render_template('index.html', title='TweetLikeTrump', user=user)
 
 @app.route('/tweet', methods=['GET', 'POST'])
 def tweet():
@@ -17,7 +22,20 @@ def tweet():
         return redirect(url_for('result'))
     return render_template('tweet.html', title = 'TweetLikeTrump - Tweet Now!', form = form)
 
-@app.route('/result')
+@app.route('/result', methods=['GET', 'POST'])
 def result():
-    form = TweetForm()
-    return render_template('result.html', title = 'TweetLikeTrump - Score', form = form)
+    if request.method == 'POST':
+        sentence = request.form['tweet']
+	
+        vect = joblib.load('vect.pkl')
+        ocsvm = joblib.load('ocsvm.pkl')
+	
+        sentence_tfidf = vect.transform([sentence])
+        ocsvm_pred = ocsvm.predict(sentence_tfidf)[0]
+
+        if ocsvm_pred == -1:
+            ocsvm_prediction = "No, you tweet not like Donald J. Trump!"
+        else:
+            ocsvm_prediction = "Hi, Mr. President!"
+
+    return render_template('result.html', title = 'TweetLikeTrump - Score', sentence = sentence, ocsvm_prediction = ocsvm_prediction)
